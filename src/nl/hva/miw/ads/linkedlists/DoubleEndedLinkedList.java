64985ae5018a06d69f0cd82f27878b71b6e53b2f
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2022-07-26 16:35:01 +0200

    DoubleEndedLinkedList

--- src/nl/hva/miw/ads/linkedlists/DoubleEndedLinkedList.java
+++ src/nl/hva/miw/ads/linkedlists/DoubleEndedLinkedList.java
@@ -3,7 +3,7 @@ package nl.hva.miw.ads.linkedlists;
 /**
  * DoubleEnded Doubly linked list.
  *
- * @author michel
+ * @author michel; Vincent Velthuizen
  */
 public class DoubleEndedLinkedList {
 
@@ -18,55 +18,109 @@ public class DoubleEndedLinkedList {
         }
     }
 
-    private int size = 0;           // Length of list
-    private Node head = null;      // Link to first node
-    private Node tail = null;      // Link to last node
+    private int size;           // Length of list
+    private Node head;      // Link to first node
+    private Node tail;      // Link to last node
 
     public DoubleEndedLinkedList() {
+        size = 0;
+        head = null;
+        tail = null;
     }
 
     public int getSize() {
         return size;
     }
 
-    /**
-     * Return the value in the list at position index.
-     *
-     * @param index
-     * @return
-     */
-    public int get( int index ) {
-        return -1;
+    private Node getNode(int index) {
+        checkIndexBounds(index);
+
+        Node current;
+        if (index > size / 2) {
+            // approach from front
+            current = head;
+
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            // approach from back
+            current = tail;
+
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+
+        return current;
     }
 
-    /**
-     * Add an element to the list at position index.
-     *
-     * @param index
-     * @param value
-     */
+    private void checkIndexBounds(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(String.format("Index: %d is out of bounds for List of size: %d",
+                    index, size));
+        }
+    }
+
+    public int get( int index ) {
+        return getNode(index).value;
+    }
+
     public void add( int index, int value ) {
         // Implement, create a new Node for this entry.
 
         Node n = new Node( value );
 
-        // Implement the rest
+        // set forward links correctly
+        if (index == 0) {
+            n.next = head;
+            head = n;
+        } else {
+            Node previous = getNode(index - 1);
+            n.next = previous.next;
+            previous.next = n;
+        }
 
+        // set backward links correctly
+        if (index == size) {
+            n.prev = tail;
+            tail = n;
+        } else {
+            //
+            Node nextNode = n.next; // this was set correctly in the code before
+            n.prev = nextNode.prev;
+            nextNode.prev = n;
+        }
+
+        size++;
     }
 
-    /**
-     * Remove an elmeent from the list at position index, if it exists.
-     *
-     * @param index
-     */
-    public void remove( int index ) {
+    public int remove( int index ) {
         // Implement, remove the corresponding node from the linked list.
+        Node toBeRemoved = getNode(index);
+
+        // Deal with forward links
+        if (index == 0) {
+            head = head.next;
+        } else {
+            toBeRemoved.prev.next = toBeRemoved.next;
+        }
+
+        // Deal with backward links
+        if (index == size - 1) {
+            tail = tail.prev;
+        } else {
+            toBeRemoved.next.prev = toBeRemoved.prev;
+        }
+
+        size--;
+        return toBeRemoved.value;
     }
 
 
     @Override
     public String toString() {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
         sb.append("DoubleEndedLinkedList{size=").append(size).append("}");
 
--- test/nl/hva/miw/ads/linkedlists/DoubleEndedLinkedListTest.java
+++ test/nl/hva/miw/ads/linkedlists/DoubleEndedLinkedListTest.java
@@ -1,15 +1,17 @@
 package nl.hva.miw.ads.linkedlists;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DoubleEndedLinkedListTest {
 
     @Test
     public void add() {
         DoubleEndedLinkedList l = new DoubleEndedLinkedList();
-        System.out.println(l);
 
         l.add(0, 400);
         l.add(0, 100);
@@ -17,8 +19,22 @@ public class DoubleEndedLinkedListTest {
         l.add(2, 300);
         l.add(4, 500);
 
-        String expected = "DoubleEndedLinkedList{size=0} 100 200 300 400 500";
+        String expected = "DoubleEndedLinkedList{size=5} 100 200 300 400 500";
         String actual = l.toString();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("removeElement")
+    void removeElement() {
+        DoubleEndedLinkedList l = new DoubleEndedLinkedList();
+
+        l.add(0, 400);
+        l.add(0, 100);
+        l.add(1, 200);
+        l.add(2, 300);
+        l.add(4, 500);
+
+        assertEquals(300, l.remove(2));
     }
 }
